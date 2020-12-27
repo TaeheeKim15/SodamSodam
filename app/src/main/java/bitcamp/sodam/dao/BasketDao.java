ef7060a1e8fb6524ba4261ffb5e10f7d27f72ccb
TaeheeKim15 <utility_maker@naver.com>
2020-12-27 18:17:18 +0900

    장바구니 수정

--- app/src/main/java/bitcamp/sodam/dao/BasketDao.java
+++ app/src/main/java/bitcamp/sodam/dao/BasketDao.java
@@ -1,15 +1,16 @@
 package bitcamp.sodam.dao;
 
 import java.util.List;
-
 import org.springframework.stereotype.Repository;
-
 import bitcamp.sodam.beans.Basket;
-import bitcamp.sodam.beans.FAQ;
 
 @Repository
 public interface BasketDao {
   List<Basket> findBasketList(int uno) throws Exception;
+  int deleteAll(int uno) throws Exception;
+  int delete(int bno) throws Exception;
+  int update(Basket basket) throws Exception;
+  int insert(Basket basket) throws Exception;
 }
 
 
--- app/src/main/java/bitcamp/sodam/service/DefaultBasketService.java
+++ app/src/main/java/bitcamp/sodam/service/DefaultBasketService.java
@@ -1,26 +1,42 @@
 package bitcamp.sodam.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import bitcamp.sodam.beans.Basket;
-import bitcamp.sodam.beans.FAQ;
-import bitcamp.sodam.beans.User;
 import bitcamp.sodam.dao.BasketDao;
-import bitcamp.sodam.dao.FAQDao;
 
 @Service
 public class DefaultBasketService implements BasketService {
 
-	@Autowired
-	BasketDao basketdao;
-	
-	@Override
-	public List<Basket> list(int uno) throws Exception {
-		List<Basket> basket = basketdao.findBasketList(uno);
-		return basket;
-	}
-	
+  @Autowired
+  BasketDao basketdao;
+
+  @Override
+  public List<Basket> list(int uno) throws Exception {
+    List<Basket> basket = basketdao.findBasketList(uno);
+    return basket;
+  }
+
+
+  @Override
+  public int deleteAll(int uno) throws Exception {
+    return basketdao.deleteAll(uno);
+  }
+
+  @Override
+  public int delete(int bno) throws Exception {
+    return basketdao.delete(bno);
+  }
+
+  @Override
+  public int update(Basket basket) throws Exception {
+    return basketdao.update(basket);
+  }
+
+  @Override
+  public int add(Basket basket) throws Exception {
+    return basketdao.insert(basket);
+  }
+
 }
