f009cc9ec46486534b36161e2527a2eb9396e057
jaehyunYi <rmrmf@naver.com>
2020-12-30 00:51:16 +0900

    .

--- app/src/main/java/bitcamp/sodam/beans/Inquiry.java
+++ app/src/main/java/bitcamp/sodam/beans/Inquiry.java
@@ -3,7 +3,6 @@ package bitcamp.sodam.beans;
 import java.util.Date;
 
 public class Inquiry {
-<<<<<<< HEAD
   private int qno;
   private int uno;
   private String title;
@@ -87,64 +86,4 @@ public class Inquiry {
   public void setAnswer(String answer) {
     this.answer = answer;
   }
-=======
-	private int qno;
-	private int uno;
-	private String title;
-	private String content;
-	private Date qdate;
-	private int qtype;
-	private int qstatus;
-	private String answer;
-	
-	public int getQno() {
-		return qno;
-	}
-	public void setQno(int qno) {
-		this.qno = qno;
-	}
-	public int getUno() {
-		return uno;
-	}
-	public void setUno(int uno) {
-		this.uno = uno;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Date getQdate() {
-		return qdate;
-	}
-	public void setQdate(Date qdate) {
-		this.qdate = qdate;
-	}
-	public int getQtype() {
-		return qtype;
-	}
-	public void setQtype(int qtype) {
-		this.qtype = qtype;
-	}
-	public int getQstatus() {
-		return qstatus;
-	}
-	public void setQstatus(int qstatus) {
-		this.qstatus = qstatus;
-	}
-	public String getAnswer() {
-		return answer;
-	}
-	public void setAnswer(String answer) {
-		this.answer = answer;
-	}
-	
->>>>>>> a7a265a7c8f84ff73c0173b8f621fff22d9b00bd
 }
