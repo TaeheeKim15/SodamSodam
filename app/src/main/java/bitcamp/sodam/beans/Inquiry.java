daadef47cf675ef3f14f2675ba3c4d36cfa098c2
jaehyunYi <rmrmf@naver.com>
2020-12-29 20:50:39 +0900

    .

--- app/src/main/java/bitcamp/sodam/beans/Inquiry.java
+++ app/src/main/java/bitcamp/sodam/beans/Inquiry.java
@@ -3,61 +3,87 @@ package bitcamp.sodam.beans;
 import java.util.Date;
 
 public class Inquiry {
-	private int qno;
-	private int uno;
-	private String title;
-	private String content;
-	private Date col;
-	private int qtype;
-	private int qstatus;
-	private String answer;
-	
-	public int getQno() {
-		return qno;
-	}
-	public void setQno(int qno) {
-		this.qno = qno;
-	}
-	public int getUno() {
-		return uno;
-	}
-	public void setUno(int uno) {
-		this.uno = uno;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Date getCol() {
-		return col;
-	}
-	public void setCol(Date col) {
-		this.col = col;
-	}
-	public int getQtype() {
-		return qtype;
-	}
-	public void setQtype(int qtype) {
-		this.qtype = qtype;
-	}
-	public int getQstatus() {
-		return qstatus;
-	}
-	public void setQstatus(int qstatus) {
-		this.qstatus = qstatus;
-	}
-	public String getAnswer() {
-		return answer;
-	}
-	public void setAnswer(String answer) {
-		this.answer = answer;
-	}
+  private int qno;
+  private int uno;
+  private String title;
+  private String content;
+  private Date col;
+  private int qtype;
+  private String type;
+  private int qstatus;
+  private String answer;
+
+  public int getQno() {
+    return qno;
+  }
+
+  public void setQno(int qno) {
+    this.qno = qno;
+  }
+
+  public int getUno() {
+    return uno;
+  }
+
+  public void setUno(int uno) {
+    this.uno = uno;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public Date getCol() {
+    return col;
+  }
+
+  public void setCol(Date col) {
+    this.col = col;
+  }
+
+  public int getQtype() {
+    return qtype;
+  }
+
+  public void setQtype(int qtype) {
+    this.qtype = qtype;
+  }
+  
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public int getQstatus() {
+    return qstatus;
+  }
+
+  public void setQstatus(int qstatus) {
+    this.qstatus = qstatus;
+  }
+
+
+    public String getAnswer() {
+    return answer;
+  }
+
+  public void setAnswer(String answer) {
+    this.answer = answer;
+  }
 }
