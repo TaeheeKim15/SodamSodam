ec3ad94a860516d91c6ea9699357dff8812bc9c6
jaehyunYi <rmrmf@naver.com>
2020-12-30 16:06:40 +0900

    .

---
+++ app/src/main/java/bitcamp/sodam/dao/OrderDao.java
@@ -0,0 +1,20 @@
+package bitcamp.sodam.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import bitcamp.sodam.beans.Order;
+import bitcamp.sodam.database.OrderMapper;
+
+@Repository
+public class OrderDao {
+	
+	@Autowired
+	private OrderMapper orderMapper;
+	
+	public List<Order> findOrderList(int uno) throws Exception{ 
+	    return orderMapper.findOrdertList(uno);
+	}
+}
---
+++ app/src/main/java/bitcamp/sodam/service/DefaultOrderService.java
@@ -0,0 +1,23 @@
+package bitcamp.sodam.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import bitcamp.sodam.beans.Order;
+import bitcamp.sodam.dao.OrderDao;
+
+
+@Service
+public class DefaultOrderService implements OrderService {
+
+	@Autowired
+	OrderDao orderDao;
+	
+	@Override
+	public List<Order> list(int uno) throws Exception {
+		List<Order> order = orderDao.findOrderList(uno);
+		return order;
+	}
+}
